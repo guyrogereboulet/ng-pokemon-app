dc1ee35a5c57c64bd06210947b49aeda598e8393
Guy Roger Eboulet <guy_eboulet_reply@whirlpool.com>
2022-10-21 11:31:05 +0200

    Add mouseEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ export class AppComponent implements OnInit {
   // Interfaccia del ciclo di vita onInit
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemon: MouseEvent) {
+    const index: number = +(event?.target as HTMLInputElement).value
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
   }
 }
